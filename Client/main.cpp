d0ca4f06e2f7cac5ae52c73b639f7c92a0989ea9
kanghwi <98413469+kanghwi@users.noreply.github.com>
2022-11-10 20:02:25 +0900

    Client에서 server_thread생성

--- Client/main.cpp
+++ Client/main.cpp
@@ -12,9 +12,9 @@ using namespace std;
 //char* SERVERIP = (char*)"127.0.0.1";
 Game game;
 
-int SDL_main(int argc, char* argv[])
+//서버 송수신용 스레드
+DWORD WINAPI server_thread(LPVOID arg)
 {
-
 	// 윈속 초기화
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
 		return 1;
@@ -23,22 +23,15 @@ int SDL_main(int argc, char* argv[])
 	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock == INVALID_SOCKET) err_quit("socket()");
 
-	cout << "Start" << endl;
-	
-	int startTime, endTime;
 
-	while (!game.done) 
-	{
-		startTime = clock();
-		game.update();
-
+	while (!game.done) {
 		if (game.curr_state == 0) {
 			if (game.connect_server && !game.server_connected) {
 				string str(game.Port);
 				int i = 0;
 				stringstream ssInt(str);
 				ssInt >> i;
-				
+
 				// connect()
 				struct sockaddr_in serveraddr;
 				memset(&serveraddr, 0, sizeof(serveraddr));
@@ -47,7 +40,7 @@ int SDL_main(int argc, char* argv[])
 				serveraddr.sin_port = htons(i);
 				retval = connect(sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
 				if (retval == SOCKET_ERROR) err_quit("connect()");
-				
+
 				game.server_connected = true; //한번만 connect하게끔
 				cout << "서버와 연결됨" << endl;
 			}
@@ -57,14 +50,35 @@ int SDL_main(int argc, char* argv[])
 				cout << "find_match 전송" << endl;
 			}
 		}
-		else {
-			
-		}
-		
+	}
+
+	return 0;
+}
+
+int SDL_main(int argc, char* argv[])
+{
+	cout << "Start" << endl;
+
+	int startTime, endTime;
+
+	//game 화면 강제 전환 (임시)
+	game.curr_state = 0;
+
+
+	HANDLE h_thread;
+	h_thread = CreateThread(NULL, 0, server_thread,
+		(LPVOID)0, 0, NULL);
+	while (!game.done)
+	{
+		startTime = clock();
+		game.update();
+
+
 		Sleep(1000 / FPS - game.delayTime);
 		endTime = clock();
 		game.delayTime = (endTime - startTime) / 1000.f;
 	}
+
 
 	//종료
 	cout << "End\n" << endl;
